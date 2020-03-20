369274b4a27cfd0dd86b7f71a63d0a3971ae29f1
Joel Jensen <superjoeljensen@optum.com>
2020-03-20 17:27:59 -0500

    changed images

--- js/analytics.js
+++ js/analytics.js
@@ -3,6 +3,6 @@
 // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 // })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-// ga('create', 'UA-1608534-17', 'auto');
+// ga('create', 'UA-XXXXXXX-XX', 'auto');
 // ga('set', 'checkProtocolTask', null);
 // ga('send', 'pageview', '/background.html');
